38b07b0a9fcebc6ff4aca50b3bfc59f90db0f65a
Chloe <cguttweb@pm.me>
2023-01-15 14:43:49 +0000

    added functionality to shiwdifferent homepage if logged in

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,6 +8,14 @@ use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
+    public function showCorrectHomepage(){
+        if (auth()->check()) {
+            return view('homepage-feed');
+        } else {
+            return view('homepage');
+        }
+    }
+
     public function register(Request $request){
         $incomingFields = $request->validate([
             'username' => ['required', 'min:3', 'max:20', Rule::unique('users', 'username')],
@@ -20,5 +28,20 @@ class UserController extends Controller
         User::create($incomingFields);
 
         return 'Hello I\'m registered!';
+    }
+
+    public function login(Request $request){
+        $incomingFields = $request->validate([
+            'loginusername' => 'required',
+            'loginpassword' => 'required'
+        ]);
+
+        if (auth()->attempt(['username' => $incomingFields['loginusername'], 'password' => $incomingFields['loginpassword']])) {
+            $request->session()->regenerate();
+            return 'It worked';
+        } else {
+            return 'Sorry your login failed';
+        }
+        
     }
 }
