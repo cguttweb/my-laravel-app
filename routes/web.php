3a280346f2aaaaa6aedce2d89f37cc73775a714c
Chloe <cguttweb@pm.me>
2023-01-15 15:36:36 +0000

    added creating posts functionality with new migration, model and controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 
 /*
@@ -15,7 +16,10 @@ use App\Http\Controllers\UserController;
 */
 
 Route::get('/', [UserController::class, "showCorrectHomepage"]);
-
 Route::post('/register', [UserController::class, "register"]);
 Route::post('/login', [UserController::class, "login"]);
 Route::post('/logout', [UserController::class, "logout"]);
+
+// blog post routes
+Route::get('/create-post', [PostController::class, 'createPost']);
+Route::post('/create-post', [PostController::class, 'storeNewPost']);
